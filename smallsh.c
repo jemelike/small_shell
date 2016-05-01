9738de2e35ec6d32f28bf3ac6372cfa025666438
jemelike <juemelike@gmail.com>
2016-04-30 21:19:23 -0400

    Update smallsh.c

--- smallsh.c
+++ smallsh.c
@@ -1,26 +1,35 @@
 /* include file for example */
 #include "smallsh.h"
-#include <string.h>
-#include <unistd.h>
-#include <signal.h>
-#include <stdio.h>
+#include "string.h"
 #include <stdlib.h>
+
+int userin(char *);
+int gettok(char **);
+int inarg(char c);
+int procline(void);
 
 /*program buffers and work pointers*/
 static char inpbuf[MAXBUF], tokbuf[2*MAXBUF], *ptr = inpbuf, *tok = tokbuf, special [] = {' ', '\t', '&', ';', '\n', '\0'};
 char *prompt = "Command> ";
-pid_t testPid;
-main()
-{	
-	static struct sigaction act, act2, act3;
+pid_t shell;
+
+int main()
+{
+	shell = getpid();
+	job_list = malloc(sizeof(struct job));
+	job_list->next = NULL;
+	
+	static struct sigaction act, act2, act3, act4;
 	
 	act.sa_handler = end_Process;
 	act2.sa_handler = stop_Process;
 	act3.sa_handler = job_remove_handle;
+	act4.sa_handler = killTerminal;
 	
 	sigaction(SIGINT, &act, NULL);
 	sigaction(SIGTSTP, &act2, NULL);
-	sigaction(SIGCHLD, &act3, NULL);
+	sigaction(SIGCHLD, &act3, NULL); 
+	sigaction(SIGQUIT, &act4, NULL); //Allows for the termination of the small shell
 	
 	while( userin(prompt) != EOF)
 		procline();
@@ -37,6 +46,7 @@ int userin(char *p)
 	
 	/* display prompt */
 	printf("%s", p);
+	
 	count = 0;
 	
 	while(1)
@@ -59,6 +69,7 @@ int userin(char *p)
 			printf("smallsh: input line too long\n");
 			count = 0;
 			printf("%s", p);
+	
 		}
 	}
 }
@@ -119,6 +130,7 @@ int procline(void){ 			/* process input line */
 	int narg;					/* number of arguments so far */
 	int type;					/* FOREGROUND or BACKGROUND */
 	
+	
 	narg = 0;
 	
 	for(;;){ 					/* loop forever */
@@ -138,29 +150,32 @@ int procline(void){ 			/* process input line */
 				
 				if(narg != 0){
 					arg[narg] = NULL;
-					
 					if(strcmp("job\0", arg[0]) == 0)
 					{
-						//printJobList(joblist);
+						printJobList(job_list);
 					}
 					else if(strcmp("bg\0",arg[0]) == 0 | strcmp("fg\0",arg[0]) ==0)
 					{
-						if(strcmp("bg\0",arg[0]) == 0)
-							setType(BACKGROUND, (pid_t)arg[1]);
-						else
-							setType(FOREGROUND, (pid_t)arg[1]);
+						if(arg[1] != NULL)
+							if(strcmp("bg\0",arg[0]))
+							{
+								adjustJob(BACKGROUND, atoi(arg[1])); 
+							}
+							else if( strcmp("fg\0",arg[0])){
+								adjustJob(FOREGROUND, atoi(arg[1])); 
+							}
 					}
 					else if(strcmp("kill\0", arg[0]) == 0)
 					{
-						kill(SIGKILL, (pid_t)arg[1]);
-					}
-					else{
+						int pid = 0 - atoi(arg[1])	;
+						kill(SIGINT, pid);
+					}else		
 						runcommand(arg, type);
-					}
+				
 				}
 				
 				if(toktype == EOL)
-					return;
+					return 0;
 				
 				narg = 0;
 				break;
@@ -172,33 +187,38 @@ int procline(void){ 			/* process input line */
 int runcommand(char **cline, int where){
 	pid_t pid;
 	int status;
-	
-	
+	struct job *new;
+
 	switch(pid = fork()){
 		case -1:
 			perror("smallsh");
 			return (-1);
-		case 0:
-			//add job to joblist
-			job *new;
-			
-			new->jobID = ++numjob;
-			new->jobID->pjob->pid = getpid();
-			if(where == FOREGROUND)
-				new->jobID->pjob->type = FOREGROUND;
-			else
-				new->jobID->pjob->type = BACKGROUND;
-				
-			new->jobID->pjob->running = 1;
-			
-			//Connect commands based on the number of pipes in the input
-			
-			addJob(new);
+		case 0:			
 			execvp(*cline, cline);
 			perror(*cline);
+			//printf("fail");
 			exit(1);
+		default:
+			new = malloc( sizeof(struct job) );
+			
+			if(where == FOREGROUND)
+				NUMFG++;
+			if(where == BACKGROUND)
+				NUMBG++;
+			
+			new->jobID = NUMFG + NUMBG;
+			new->pjob = malloc(sizeof(struct job));
+			new->pjob->pid = pid;
+			new->pjob->file = *cline;
+			if(where == FOREGROUND)
+				new->pjob->type = FOREGROUND;
+			else
+				new->pjob->type = BACKGROUND;
+			
+			new->pjob->running = RUNNING;
+			addJob(new);
 	}
-	testPid = pid;
+	
 	/* code for parent */
 	/* if background process print pid and exit */
 	if(where == BACKGROUND){
@@ -209,22 +229,30 @@ int runcommand(char **cline, int where){
 	/* wait until process pid exits */
 	if(waitpid(pid, &status, 0) == -1)
 		return (-1);
-	
 	else
 		return (status);
 }
-//use waitpid to determine whether job has finished
-//sigcon for bg
-void setType(int type, pid_t pid)
+
+void end_Process(int sig)
 {
+	int j ;
+
+	if(NUMFG == 0)
+	 {
+		 printf("^C\n");
+		system("./smallsh.exe");
+	 }else {
+		if((j= findFG()) == -1)
+			kill(SIGKILL,j);
+		else
+		{
+			printf("^C\n");
+			system("./smallsh.exe");
+		}
+	 }
 }
 
-void end_Process()
-{
-	kill(SIGKILL,findFG());
-}
-
-void stop_Process()
+void stop_Process(int sig)
 {
 	pid_t pid;
 	pid = findFG();
@@ -232,27 +260,104 @@ void stop_Process()
 	kill(SIGTSTP,pid);
 }
 
-
 void addJob(struct job* new)
 {
 	struct job *current;
 	 
-	if(job_list == NULL)
-		job_list = new;
+	if(job_list->next == NULL)
+		job_list->next = new;
 	else	 
-		for( current = job_list; current != NULL ;current = current->next)
+		for( current = job_list->next; current != NULL ;current = current->next)
 			if(current->next == NULL){
 				current->next = new;
 				break;
 			}
 }
 
-void job_remove_handle(){
-	
-}
-
 pid_t findFG(){
-	for(struct job* current = job_list; current != NULL; current = current->next)
+	if(job_list->next == (struct job*)NULL)
+	 {
+		printf("No Jobs\n");
+		//return -1;
+	 }
+	for(struct job* current = job_list->next; current != NULL; current = current->next)
 		if(current->pjob->type == FOREGROUND)
 			return current->pjob->pid;
 }	
+
+void printJobList(){
+	
+	 struct job *current;
+	 
+	 if(job_list->next == (struct job*)NULL)
+	 {
+		printf("JID PID TYPE RUNNING \n");
+		return;
+	 }
+	 printf("JID PID TYPE RUNNING \n");
+	 for( current = job_list->next; current != NULL ;current = current->next)
+			if(current != NULL && current->pjob->type == BACKGROUND)
+				printf("%d %3d %d %d\n", current->jobID, current->pjob->pid, current->pjob->type, current->pjob->running);
+}
+
+
+void job_remove_handle(int sig){
+	struct job *current, *temp, *trail;
+	int status;
+
+	if(NUMBG <= 0 || NUMFG <= 0)
+		return;
+	
+	for(current = job_list -> next, trail = job_list; current != NULL; current = current->next)
+	{
+		if(waitpid(current->pjob->pid,&status,0) == -1 ){
+		
+			if(current->pjob->running < STOPPED || current->pjob->running > RUNNING )
+			{
+				temp = current;
+				trail->next =current->next;
+				current = current->next;
+			
+				if(current->pjob->type == FOREGROUND)
+					NUMFG--;
+				if(current->pjob->type == BACKGROUND)
+					NUMBG--;
+				free(temp);
+			}	
+			break;
+			
+		}
+		trail = current;
+	}
+}
+
+void killTerminal(int sig)
+{
+	kill(SIGKILL, shell);
+}
+
+	
+void adjustJob(int where,int jobID ){
+	
+	for(struct job *current = job_list->next; current != NULL; current = current->next)
+	{
+		if(current->jobID == jobID)
+		{
+			if(where == BACKGROUND)
+			{
+				current->pjob->running = RUNNING;
+				kill(SIGCONT, current->pjob->pid);
+			}
+			else if(where == FOREGROUND)
+			{
+				
+				current->pjob->running = RUNNING;
+				current->pjob->type = where;
+				kill(SIGCONT, current->pjob->pid);
+			}
+			
+			break;
+		}
+	}
+	
+}
